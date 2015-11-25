fd96350606a9440c6e2ed6bce26b83ec16c01af0
Mike Resoli <mike.resoli@gmail.com>
2015-11-25 13:42:30 +0000

    Major changes
    Major changes made to .js files and minor changes made to styling and
    mark-up.

--- js.js
+++ js.js
@@ -1,56 +1,84 @@
 function createTable() {
+	if ($('table').length <= 0) {
+		if ($('.csv').val().length > 0) {
+		    // Get the data
+			var excelData = document.getElementsByClassName('csv')[0].value;
+			
+		    // split into rows. Charcode 10 = new line
+		    excelRow = excelData.split(String.fromCharCode(10));
 
-    // Get the data
-	var excelData = document.getElementById('csv').value;
-	
-    // split into rows. Charcode 10 = new line
-    excelRow = excelData.split(String.fromCharCode(10));
+		    // split rows into columns. Charcode 9 = tab
+		    for (i=0; i<excelRow.length; i++) {
+		        excelRow[i] = excelRow[i].split(String.fromCharCode(9));
 
-    // split rows into columns. Charcode 9 = tab
-    for (i=0; i<excelRow.length; i++) {
-        excelRow[i] = excelRow[i].split(String.fromCharCode(9));
+		    }
+			
+			// start to create the HTML table
+			var tableDiv = document.createElement("div");
+			var myTable = document.createElement("table");
+			var myTbody = document.createElement("tbody");
 
-    }
-	
-	// start to create the HTML table
-	var myTable = document.createElement("table");
-	myTable.className = "table";
-	var myTbody = document.createElement("tbody");
-	
-	// Loop over the rows
-    for (i=0; i<excelRow.length - 1; i++) {
+			tableDiv.className = "table-responsive";
+			myTable.className = "table";
+			
+			// Loop over the rows
+		    for (i=0; i<excelRow.length - 1; i++) {
 
-		// create a row in the HTML table
-		var myRow = document.createElement("tr");
-		
-		// Loop over the columns and add TD to the TR
-		for (j=0; j<excelRow[i].length; j++) {
-			// Loop over the row columns
-			if (excelRow[i][j].length != 0) {
-					var myCell = document.createElement("td");
-					myCell.innerHTML = excelRow[i][j];
-            }			
-			myRow.appendChild(myCell);			
+				// create a row in the HTML table
+				var myRow = document.createElement("tr");
+				
+				// Loop over the columns and add TD to the TR
+				for (j=0; j<excelRow[i].length; j++) {
+					// Loop over the row columns
+					if (excelRow[i][j].length != 0) {
+							var myCell = document.createElement("td");
+							myCell.innerHTML = excelRow[i][j];
+		            }			
+					myRow.appendChild(myCell);			
+				}
+				myTbody.appendChild(myRow);	
+			}
+				myTable.appendChild(myTbody);
+
+		    document.getElementsByClassName('container')[0].appendChild(tableDiv);
+		    document.getElementsByClassName('table-responsive')[0].appendChild(myTable);    
+
+		    document.getElementsByClassName('csv')[0].value = '';
+		    $('tr:nth-child(1)').addClass('header');
+		} else {
+			alert('To create a table please enter CSV data.')
 		}
-		myTbody.appendChild(myRow);	
+	} else {
+		alert('Please remove current table first.')
 	}
-		myTable.appendChild(myTbody);
-
-    document.getElementsByClassName('container')[0].appendChild(myTable);
-
-    makeEditable();
-    document.getElementById('csv').value = '';
-    $('.alert').show();
 }
 
 function removeTable(){
-	if ($('.table').length > 0) {
-		$('.table').remove();
+	if ($('table').length > 0) {
+		$('.table-responsive').remove();
 	} else {
 		alert('To remove a table one must exist.')
 	}
 }
 
 function copyTable(){
-	alert('Copy table')
+	if ($('table').length > 0) {
+		var table = $('table').parent().html();
+		var tableString = table.replace('class="table"','');
+
+	} else {
+		alert('To copy a table one must exist.')
+	}
+}
+
+function toggleCss(){
+	var table = $('table')
+
+	if ($('table').hasClass('table')) {
+		$(table).removeClass('table')
+		$(table).find('tr:nth-child(1)').removeClass('header')
+	} else {
+		$(table).addClass('table')
+		$(table).find('tr:nth-child(1)').addClass('header')
+	}
 }
